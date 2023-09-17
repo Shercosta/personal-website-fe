edf64088213cbbd926553369653634e92a26e9e8
Geizka Rozilia Ruicosta <70560582+Shercosta@users.noreply.github.com>
2023-09-17 20:06:21 +0700

    Navbar design OK

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -8,17 +8,17 @@ function Navigation() {
     <Navbar fixed="top" expand="lg" className="bg-body-tertiary">
       <Container className="">
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
+        <Navbar.Collapse id="basic-navbar-nav" className="order-2 order-lg-0">
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#work">Work</Nav.Link>
             <Nav.Link href="#contact">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
-        <Navbar.Brand>
+        <Navbar.Brand className="order-1 order-lg-0">
           <FaSun></FaSun>
         </Navbar.Brand>
-        <Navbar.Collapse id="basic-navbar-nav">
+        <Navbar.Collapse id="basic-navbar-nav" className="order-3 order-lg-0">
           <Nav className="ms-auto">
             <Nav.Link>
               <FaInstagram></FaInstagram>
