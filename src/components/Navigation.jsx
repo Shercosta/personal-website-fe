5547b16b862aee3635f3d3cf4e1ea40945e9d466
Geizka Rozilia Ruicosta <70560582+Shercosta@users.noreply.github.com>
2023-09-19 05:00:57 +0700

    fix: convert from original format

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -9,41 +9,60 @@ function Navigation() {
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container className="">
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav" className="order-2 order-lg-0">
-          <Nav className="me-auto">
-            <Nav.Link>
-              <Link to={"/"} className="text-reset text-decoration-none">
-                Home
-              </Link>
-            </Nav.Link>
-            <Nav.Link>
-              <Link to={"work"} className="text-reset text-decoration-none">
-                Work
-              </Link>
-            </Nav.Link>
-            <Nav.Link>
-              <Link to={"Contact"} className="text-reset text-decoration-none">
-                Contact
-              </Link>
-            </Nav.Link>
-          </Nav>
+        <Navbar.Collapse
+          id="basic-navbar-nav"
+          className="text-center row"
+          style={{ marginLeft: 0, marginRight: 0 }}
+        >
+          <div className="col-md-4">
+            <Nav className="me-auto">
+              <Nav.Link>
+                <Link to={"/"} className="text-reset text-decoration-none">
+                  Home
+                </Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link to={"work"} className="text-reset text-decoration-none">
+                  Work
+                </Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link
+                  to={"Contact"}
+                  className="text-reset text-decoration-none"
+                >
+                  Contact
+                </Link>
+              </Nav.Link>
+            </Nav>
+          </div>
+          <div className="col-md-4">
+            <div className="mx-auto order-0">
+              <div>
+                <Navbar.Brand className="order-1 order-lg-0">
+                  <FaSun></FaSun>
+                </Navbar.Brand>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <Nav className="ms-auto">
+              <Nav.Link>
+                <FaInstagram></FaInstagram>
+              </Nav.Link>
+              <Nav.Link>
+                <FaTiktok></FaTiktok>
+              </Nav.Link>
+              <Nav.Link>
+                <FaLinkedin></FaLinkedin>
+              </Nav.Link>
+            </Nav>
+          </div>
         </Navbar.Collapse>
-        <Navbar.Brand className="order-1 order-lg-0">
-          <FaSun></FaSun>
-        </Navbar.Brand>
-        <Navbar.Collapse id="basic-navbar-nav" className="order-3 order-lg-0">
-          <Nav className="ms-auto">
-            <Nav.Link>
-              <FaInstagram></FaInstagram>
-            </Nav.Link>
-            <Nav.Link>
-              <FaTiktok></FaTiktok>
-            </Nav.Link>
-            <Nav.Link>
-              <FaLinkedin></FaLinkedin>
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+        {/* <Navbar.Collapse
+          id="basic-navbar-nav"
+          className="order-3 order-lg-0"
+        ></Navbar.Collapse> */}
       </Container>
     </Navbar>
   );
