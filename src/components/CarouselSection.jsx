fb23578b006cc2452c4400e5d11b8628c252e011
Geizka Rozilia Ruicosta <70560582+Shercosta@users.noreply.github.com>
2023-09-26 04:10:27 +0700

    feat: new carousel

--- src/components/CarouselSection.jsx
+++ src/components/CarouselSection.jsx
@@ -1,19 +1,34 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
+import { Button } from "react-bootstrap";
+import { useSelector } from "react-redux";
 
 const CarouselSection = ({ contents }) => {
+  const getTheme = useSelector((state) => state.themeSlice);
+  const buttonVariant = (theme) => {
+    return theme === "light" ? "dark" : "light";
+  };
+
   return (
-    <Carousel>
+    <Carousel fade controls={false} pause="hover">
       {contents.map((val) => {
         return (
           <Carousel.Item>
             <div className="carousel-parent">
-              <img src={val.image} className="img-carousel" />
+              <div className="carousel-text">
+                <h3>{val.label}</h3>
+                <p>{val.caption}</p>
+                <Button variant={buttonVariant(getTheme)}>Go</Button>
+              </div>
+              <div className="carousel-image">
+                <img src={val.image} className="image-carousel" />
+                {/* <hr /> */}
+              </div>
             </div>
-            <Carousel.Caption>
+            {/* <Carousel.Caption>
               <h3>{val.label}</h3>
               <p>{val.caption}</p>
-            </Carousel.Caption>
+            </Carousel.Caption> */}
           </Carousel.Item>
         );
       })}
