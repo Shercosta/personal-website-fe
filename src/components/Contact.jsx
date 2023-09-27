36351932c16af85a1340eef9f77fd75bdf5526b5
Geizka Rozilia Ruicosta <70560582+Shercosta@users.noreply.github.com>
2023-09-27 07:33:46 +0700

    feat: contact alert use state

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container } from "react-bootstrap";
 import { FaRocket, FaWhatsapp } from "react-icons/fa6";
 import { MdOutlineEmail } from "react-icons/md";
 
 const Contact = () => {
+  const [visibility, setVisibility] = useState("contact-details invisible");
+  //   let alertVisibility = "invisible";
+  //   let alertClass = `contact-details ${alertVisibility}`;
+
+  const handleClick = () => {
+    setVisibility("contact-details visible");
+  };
   return (
     <Container>
       <div className="center-screen d-flex align-items-center text-center">
@@ -16,21 +23,23 @@ const Contact = () => {
                 Your Future Fullstack Web Developer Awaits.
               </h1>
             </div>
-            <div className="col hr-box">
+            <div className="col-sm-12 col-md-12 col-lg-5 hr-box">
               <hr className="hr-middle" />
             </div>
-            <div className="col-1">
+            <div className="col-sm-12 col-lg-2">
               <p className="contact-p">
-                <FaWhatsapp />
-              </p>
-            </div>
-            <div className="col-1">
-              <p className="contact-p">
+                <FaWhatsapp onClick={handleClick} />
+                <span className="contact-icon" />
                 <MdOutlineEmail />
               </p>
             </div>
-            <div className="col hr-box">
+            <div className="col-sm-12 col-md-12 col-lg-5 hr-box">
               <hr className="hr-middle" />
+            </div>
+          </div>
+          <div className={visibility}>
+            <div class="alert alert-info" role="alert">
+              A simple info alert—check it out!
             </div>
           </div>
         </Container>
