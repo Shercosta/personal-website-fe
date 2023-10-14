80be89d81fe5419e90e8d77cb2ee0abe7e7f2936
Geizka Rozilia Ruicosta <70560582+Shercosta@users.noreply.github.com>
2023-10-14 23:04:02 +0700

    fix: correct size logo picture

--- src/components/LogoCarousel.jsx
+++ src/components/LogoCarousel.jsx
@@ -3,11 +3,11 @@ import React from "react";
 const LogoCarousel = () => {
   return (
     <div className="ms-auto">
-      <div className="position-relative">
-        <div className="position-absolute z-3">
-          <img src="/public/Tech-holder.svg" className="logo-picture" />
+      <div className="position-relative box-relative">
+        <div className="position-absolute box-absolute z-3">
+          <img src="/public/Tech-holder.svg" className="w-100 h-100" />
         </div>
-        <div className="position-absolute"></div>
+        <div className="position-absolute box-absolute"></div>
       </div>
     </div>
   );
