56a47d950af60e740cf04eb3012d3fcae6b13714
Geizka Rozilia Ruicosta <70560582+Shercosta@users.noreply.github.com>
2024-01-30 17:49:51 +0700

    add animation to skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,72 +1,64 @@
 import React from "react";
-import logos from "../arrays/techs";
 import TableSkills from "./TableSkills";
+import { Fade } from "react-awesome-reveal";
 
 const Skills = () => {
-  //   logos.sort((logo1, logo2) =>
-  //     logo1.level > logo2.level ? -1 : logo1.level < logo2.level ? 1 : 0
-  //   );
-  //   logos.sort((logo1, logo2) =>
-  //     logo1.used > logo2.used ? -1 : logo1.used < logo2.used ? 1 : 0
-  //   );
-
-  //   logos.sort((logo1, logo2) => {
-  //     if (logo1.used > logo2.used) {
-  //       -1;
-  //     } else if (logo1.used < logo2.used) {
-  //       1;
-  //     } else {
-  //       0;
-  //     }
-  //   });
-
-  //   console.log(logos);
-
   return (
     <>
       <div className="row text-center me-0">
         <div className="col">
-          <h4 className="d-inline-block">Most Used</h4>
+          <Fade direction="left">
+            <h4 className="d-inline-block">Most Used</h4>
+          </Fade>
         </div>
         <div className="col">
-          <hr className="hr-middle" />
+          <Fade direction="left" delay={200}>
+            <hr className="hr-middle" />
+          </Fade>
         </div>
         <div className="col">
-          <h4 className="d-inline-block">Less Used</h4>
+          <Fade direction="left" delay={400}>
+            <h4 className="d-inline-block">Less Used</h4>
+          </Fade>
         </div>
       </div>
-      <div className="row text-center me-0">
-        <div className="col-2">
-          <div>
-            <h4>Expert Level</h4>
+      <Fade direction="down">
+        <div className="row text-center me-0">
+          <div className="col-2">
+            <div>
+              <h4>Expert Level</h4>
+            </div>
+
+            <div class="vr svh100"></div>
+            <div>
+              <h4>Basic Level</h4>
+            </div>
           </div>
-          <div class="vr svh100"></div>
-          <div>
-            <h4>Basic Level</h4>
+          <div className="col-10">
+            <Fade delay={700}>
+              <table className="text-start">
+                <tbody>
+                  <tr>
+                    <TableSkills identifier={5} />
+                  </tr>
+                  <tr>
+                    <TableSkills identifier={4} />
+                  </tr>
+                  <tr>
+                    <TableSkills identifier={3} />
+                  </tr>
+                  <tr>
+                    <TableSkills identifier={2} />
+                  </tr>
+                  <tr>
+                    <TableSkills identifier={1} />
+                  </tr>
+                </tbody>
+              </table>
+            </Fade>
           </div>
         </div>
-        <div className="col-10">
-          <table className="text-start">
-            <tbody>
-              <tr>
-                <TableSkills identifier={5} />
-              </tr>
-              <tr>
-                <TableSkills identifier={4} />
-              </tr>
-              <tr>
-                <TableSkills identifier={3} />
-              </tr>
-              <tr>
-                <TableSkills identifier={2} />
-              </tr>
-              <tr>
-                <TableSkills identifier={1} />
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
+      </Fade>
     </>
   );
 };
