09e093fa2811ea8c24e92bde472ff76f6a45adcc
Geizka Rozilia Ruicosta <70560582+Shercosta@users.noreply.github.com>
2024-01-30 14:58:27 +0700

    sorted but not shown entirely

--- src/arrays/techs.js
+++ src/arrays/techs.js
@@ -3,97 +3,137 @@ const logos = [
     logoUrl:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png",
     logoName: "JavaScript",
+    used: 5,
+    level: 5,
   },
   {
     logoUrl:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1200px-Python-logo-notext.svg.png",
     logoName: "Python",
+    used: 2,
+    level: 2,
   },
   {
     logoUrl:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/800px-ISO_C%2B%2B_Logo.svg.png",
     logoName: "C++",
+    used: 3,
+    level: 2,
   },
   {
     logoUrl:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png",
     logoName: "React.js",
+    used: 4,
+    level: 4,
   },
   {
     logoUrl:
       "https://miro.medium.com/v2/resize:fit:800/1*bc9pmTiyKR0WNPka2w3e0Q.png",
     logoName: "Node.js",
+    used: 5,
+    level: 5,
   },
   {
     logoUrl:
       "https://cdn.icon-icons.com/icons2/2107/PNG/512/file_type_ejs_icon_130626.png",
     logoName: "EJS",
+    used: 5,
+    level: 5,
   },
   {
     logoUrl:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/640px-HTML5_logo_and_wordmark.svg.png",
     logoName: "HTML5",
+    used: 5,
+    level: 5,
   },
   {
     logoUrl:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1200px-CSS3_logo_and_wordmark.svg.png",
     logoName: "CSS3",
+    used: 5,
+    level: 5,
   },
   {
     logoUrl:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/800px-Bootstrap_logo.svg.png",
     logoName: "Bootstrap",
+    used: 5,
+    level: 5,
   },
   {
     logoUrl: "https://cdn.worldvectorlogo.com/logos/redux.svg",
     logoName: "Redux",
+    used: 2,
+    level: 5,
   },
   {
     logoUrl:
       "https://assets.asana.biz/transform/ba9b63a3-f255-4088-b5fe-14ab4628f50b/logo-app-figma",
     logoName: "Figma",
+    used: 4,
+    level: 3,
   },
   {
     logoUrl:
       "https://styles.redditmedia.com/t5_2qm6k/styles/communityIcon_dhjr6guc03x51.png",
     logoName: "MySQL",
+    used: 1,
+    level: 3,
   },
   {
     logoUrl:
       "https://miro.medium.com/v2/resize:fit:512/1*doAg1_fMQKWFoub-6gwUiQ.png",
     logoName: "MongoDB",
+    used: 4,
+    level: 4,
   },
   {
     logoUrl:
       "https://balsamiq.com/assets/company/brandassets/smileyface-transparent-1080x1080.png",
     logoName: "Balsamiq",
+    used: 5,
+    level: 4,
   },
   {
     logoUrl: "https://1000logos.net/wp-content/uploads/2020/08/Django-Logo.png",
     logoName: "Django",
+    used: 1,
+    level: 1,
   },
   {
     logoUrl:
       "https://plugins.jetbrains.com/files/20588/418328/icon/pluginIcon.svg",
     logoName: "HTMX",
+    used: 1,
+    level: 3,
   },
   {
     logoUrl: "https://sass-lang.com/assets/img/styleguide/seal-color.png",
     logoName: "SASS",
+    used: 1,
+    level: 3,
   },
   {
     logoUrl:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Postgresql_elephant.svg/1200px-Postgresql_elephant.svg.png",
     logoName: "PostgreSQL",
+    used: 1,
+    level: 4,
   },
   {
     logoUrl: "https://cdn.worldvectorlogo.com/logos/next-js.svg",
     logoName: "Next.js",
+    used: 1,
+    level: 4,
   },
   {
     logoUrl:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1200px-Vue.js_Logo_2.svg.png",
     logoName: "Vue.js",
+    used: 1,
+    level: 4,
   },
 ];
 
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,6 +1,64 @@
 import React from "react";
+import logos from "../arrays/techs";
 
 const Skills = () => {
+  logos.sort((logo1, logo2) =>
+    logo1.level > logo2.level ? -1 : logo1.level < logo2.level ? 1 : 0
+  );
+  logos.sort((logo1, logo2) =>
+    logo1.used > logo2.used ? -1 : logo1.used < logo2.used ? 1 : 0
+  );
+
+  //   logos.sort((logo1, logo2) => {
+  //     if (logo1.used > logo2.used) {
+  //       -1;
+  //     } else if (logo1.used < logo2.used) {
+  //       1;
+  //     } else {
+  //       0;
+  //     }
+  //   });
+
+  //   console.log(logos);
+
+  //   let skillLevel = null;
+
+  //   const getSortedLogos = logos.map((logo, idx) => {
+  //     if (idx === 0) {
+  //       skillLevel = 5;
+  //     } else {
+  //         if (logo.level === skillLevel) {
+  //             return (
+  //                 <>
+  //                     <td></td>
+  //                 </>
+  //             )
+  //         }
+  //     }
+
+  //     return (
+  //       <>
+  //         <p>
+  //           {logo.level} - {logo.used}
+  //         </p>
+  //       </>
+  //     );
+  //   });
+
+  const getSortedLogos = () => {
+    for (let logoLevel = 5; logoLevel > 0; logoLevel--) {
+      for (let logoUsed = 5; logoUsed > 0; logoUsed--) {
+        return (
+          <>
+            <td>
+              {logos[logoLevel].level}-{logos[logoUsed].used}
+            </td>
+          </>
+        );
+      }
+    }
+  };
+
   return (
     <>
       <div className="row text-center me-0">
@@ -26,12 +84,7 @@ const Skills = () => {
         </div>
         <div className="col">
           <table>
-            <thead>
-              <tr>
-                <th>Most Used</th>
-                <th>Less Used</th>
-              </tr>
-            </thead>
+            <tbody>{getSortedLogos()}</tbody>
           </table>
         </div>
       </div>
