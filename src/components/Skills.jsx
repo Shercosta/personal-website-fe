411dd499a248ab9037207da68b1faaff8e69abce
Geizka Rozilia Ruicosta <70560582+Shercosta@users.noreply.github.com>
2024-01-30 13:40:50 +0700

    feat: create line graph

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,7 +1,42 @@
 import React from "react";
 
 const Skills = () => {
-  return <div>Skills</div>;
+  return (
+    <>
+      <div className="row text-center">
+        <div className="col">
+          <h4 className="d-inline-block">Most Used</h4>
+        </div>
+        <div className="col">
+          <hr className="hr-middle" />
+        </div>
+        <div className="col">
+          <h4 className="d-inline-block">Less Used</h4>
+        </div>
+      </div>
+      <div className="row text-center">
+        <div className="col-2">
+          <div>
+            <h4>Expert Level</h4>
+          </div>
+          <div class="vr svh100"></div>
+          <div>
+            <h4>Basic Level</h4>
+          </div>
+        </div>
+        <div className="col">
+          <table>
+            <thead>
+              <tr>
+                <th>Most Used</th>
+                <th>Less Used</th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Skills;
