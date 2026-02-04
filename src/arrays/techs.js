fa0d24cf30dd95caab4b1a89da5a2eb976c6438a
Shercosta <shercostagp@gmail.com>
2026-02-04 09:12:36 +0700

    feat: reorder techs js

--- src/arrays/techs.js
+++ src/arrays/techs.js
@@ -8,20 +8,6 @@ const logos = [
   },
   {
     logoUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1200px-Python-logo-notext.svg.png",
-    logoName: "Python",
-    used: 2,
-    level: 2,
-  },
-  {
-    logoUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/800px-ISO_C%2B%2B_Logo.svg.png",
-    logoName: "C++",
-    used: 3,
-    level: 2,
-  },
-  {
-    logoUrl:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png",
     logoName: "React.js",
     used: 4,
@@ -33,6 +19,88 @@ const logos = [
     logoName: "Node.js",
     used: 5,
     level: 5,
+  },
+  {
+    logoUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Postgresql_elephant.svg/1200px-Postgresql_elephant.svg.png",
+    logoName: "PostgreSQL",
+    used: 5,
+    level: 5,
+  },
+  {
+    logoUrl: "https://cdn.worldvectorlogo.com/logos/next-js.svg",
+    logoName: "Next.js",
+    used: 5,
+    level: 5,
+  },
+  {
+    logoUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1200px-Vue.js_Logo_2.svg.png",
+    logoName: "Vue.js",
+    used: 4,
+    level: 3,
+  },
+  {
+    logoUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/1200px-Typescript_logo_2020.svg.png",
+    logoName: "TypeScript",
+    used: 5,
+    level: 5,
+  },
+  {
+    logoUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Laravel.svg/1200px-Laravel.svg.png",
+    logoName: "Laravel",
+    used: 5,
+    level: 5,
+  },
+  {
+    logoUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Go_Logo_Blue.svg/1200px-Go_Logo_Blue.svg.png",
+    logoName: "Go",
+    used: 5,
+    level: 4,
+  },
+  {
+    logoUrl:
+      "https://styles.redditmedia.com/t5_2qm6k/styles/communityIcon_dhjr6guc03x51.png",
+    logoName: "MySQL",
+    used: 1,
+    level: 3,
+  },
+  {
+    logoUrl:
+      "https://miro.medium.com/v2/resize:fit:512/1*doAg1_fMQKWFoub-6gwUiQ.png",
+    logoName: "MongoDB",
+    used: 4,
+    level: 4,
+  },
+  {
+    logoUrl: "https://upload.wikimedia.org/wikipedia/commons/a/a8/NestJS.svg",
+    logoName: "Nest Js",
+    used: 4,
+    level: 5,
+  },
+  {
+    logoUrl:
+      "https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/001/287/square_480/prismaHD.png",
+    logoName: "Prisma",
+    used: 5,
+    level: 5,
+  },
+  {
+    logoUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1200px-Python-logo-notext.svg.png",
+    logoName: "Python",
+    used: 2,
+    level: 2,
+  },
+  {
+    logoUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/800px-ISO_C%2B%2B_Logo.svg.png",
+    logoName: "C++",
+    used: 3,
+    level: 2,
   },
   {
     logoUrl:
@@ -77,20 +145,6 @@ const logos = [
   },
   {
     logoUrl:
-      "https://styles.redditmedia.com/t5_2qm6k/styles/communityIcon_dhjr6guc03x51.png",
-    logoName: "MySQL",
-    used: 1,
-    level: 3,
-  },
-  {
-    logoUrl:
-      "https://miro.medium.com/v2/resize:fit:512/1*doAg1_fMQKWFoub-6gwUiQ.png",
-    logoName: "MongoDB",
-    used: 4,
-    level: 4,
-  },
-  {
-    logoUrl:
       "https://images.sftcdn.net/images/t_app-icon-m/p/d61d2782-b12f-4919-b266-11705619e2c9/2739762855/balsamiq-mockups-logo.png",
     logoName: "Balsamiq",
     used: 5,
@@ -116,26 +170,6 @@ const logos = [
     level: 3,
   },
   {
-    logoUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Postgresql_elephant.svg/1200px-Postgresql_elephant.svg.png",
-    logoName: "PostgreSQL",
-    used: 5,
-    level: 5,
-  },
-  {
-    logoUrl: "https://cdn.worldvectorlogo.com/logos/next-js.svg",
-    logoName: "Next.js",
-    used: 5,
-    level: 5,
-  },
-  {
-    logoUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1200px-Vue.js_Logo_2.svg.png",
-    logoName: "Vue.js",
-    used: 4,
-    level: 3,
-  },
-  {
     logoUrl: "https://avatars.githubusercontent.com/u/7552965?s=280&v=4",
     logoName: "Mongoose js",
     used: 3,
@@ -156,27 +190,6 @@ const logos = [
     level: 5,
   },
   {
-    logoUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/1200px-Typescript_logo_2020.svg.png",
-    logoName: "TypeScript",
-    used: 5,
-    level: 5,
-  },
-  {
-    logoUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Laravel.svg/1200px-Laravel.svg.png",
-    logoName: "Laravel",
-    used: 5,
-    level: 5,
-  },
-  {
-    logoUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Go_Logo_Blue.svg/1200px-Go_Logo_Blue.svg.png",
-    logoName: "Go",
-    used: 5,
-    level: 4,
-  },
-  {
     logoUrl: "https://cdn.worldvectorlogo.com/logos/tailwindcss.svg",
     logoName: "Tailwind CSS",
     used: 5,
@@ -194,19 +207,6 @@ const logos = [
     logoName: "React Native",
     used: 3,
     level: 4,
-  },
-  {
-    logoUrl: "https://upload.wikimedia.org/wikipedia/commons/a/a8/NestJS.svg",
-    logoName: "Nest Js",
-    used: 4,
-    level: 5,
-  },
-  {
-    logoUrl:
-      "https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/001/287/square_480/prismaHD.png",
-    logoName: "Prisma",
-    used: 5,
-    level: 5,
   },
   {
     logoUrl:
