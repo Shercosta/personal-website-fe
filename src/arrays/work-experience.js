baff7a183b44fc113c9b210b018c2e1482c448b1
Shercosta <shercostagp@gmail.com>
2026-03-06 12:23:19 +0700

    chore: work experience js

---
+++ src/arrays/work-experience.js
@@ -0,0 +1,46 @@
+let work_experiences = [
+  {
+    id: 1,
+    companyName: "Vesperia",
+    startMonth: "3",
+    startYear: "2024",
+    endMonth: "8",
+    endYear: "2025",
+    outSourcedAt: [],
+  },
+  {
+    id: 2,
+    companyName: "DRX Asia",
+    startMonth: "7",
+    startYear: "2025",
+    endMonth: "1",
+    endYear: "2026",
+    outSourcedAt: [],
+  },
+  {
+    id: 3,
+    companyName: "Deptech",
+    startMonth: "2",
+    startYear: "2026",
+    outSourcedAt: [
+      {
+        id: 1,
+        companyName: "Bank Tabungan Negara",
+        startMonth: "2",
+        startYear: "2026",
+        endMonth: "8",
+        endYear: "2026",
+      },
+      {
+        id: 2,
+        companyName: "Bank Central Asia",
+        startMonth: "9",
+        startYear: "2026",
+        endMonth: null,
+        endYear: null,
+      },
+    ],
+  },
+];
+
+export default work_experiences;
