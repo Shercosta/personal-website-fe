c4f42fb70c91fd5127a225e6d9294b6b8df8d835
Shercosta <shercostagp@gmail.com>
2026-03-31 14:56:48 +0700

    feat: add resume js

---
+++ src/arrays/resume.js
@@ -0,0 +1,83 @@
+const resume = {
+  name: "Geizka Rozilia Ruicosta",
+  suffix: "S. Kom",
+  skillHighlights: ["Fullstack", "Frontend", "Backend", "Software Engineer"],
+  aboutMeSection: {
+    title: "About Me",
+    description:
+      "A versatile, goal-driven Software Engineer with expertise in developing, improving, and maintaining full-stack software systems. My experience spans frontend and backend development, API integration, and database management across various industries. I am proficient in technologies such as React, Vue, Laravel, Node.js, and Go, with hands-on experience in cloud-based architectures, microservices, and containerized environments using Docker. I am passionate about building scalable, high-performance software solutions, whether for web applications, enterprise systems, or real-time platforms.",
+  },
+  workingExperiencesSection: {
+    title: "Working Experiences",
+    items: [
+      {
+        companyName: "PT Deptech Digital Indonesia",
+        companies: [
+          {
+            companyName: "PT Bank Tabungan Negara (Persero) Tbk",
+            companyAlias: "BTN",
+            jobTitle: "Fullstack Developer",
+            startDate: new Date("2026-02-01"),
+            responsibilities: [
+              "Develop and maintain APIs",
+              "Implement microservices architecture in backend development for application practice and scalability",
+              "Convert Figma design to frontend application with feasible UI/UX",
+              "Integrate API for seamless data communication and optimized application functions",
+              "Follows best practice throughout cycle from development to deployment",
+            ],
+          },
+        ],
+      },
+      {
+        companyName: "PT Berkat Indah Garment",
+        companyAlias: "DRX",
+        jobTitle: "Fullstack Developer",
+        startDate: new Date("2025-07-01"),
+        endDate: new Date("2026-01-31"),
+        responsibilities: [
+          "Develop and implement new features for both mobile and web applications.",
+          "Collaborate with business stakeholders to define requirements and prioritize future features.",
+          "Integrate third-party APIs and applications to support marketing initiatives.",
+          "Design and refactor APIs and codebases to enhance reusability and maintainability.",
+          "Lead other developers.",
+          "Manage task allocation.",
+          "Participate in the recruitment process for new developers and engineers, including creating assessment tests for frontend, backend, and fullstack hires.",
+        ],
+      },
+      {
+        companyName: "PT Vesperia Global Merdeka",
+        jobTitle: "Software Engineer",
+        startDate: new Date("2024-03-01"),
+        endDate: new Date("2025-07-31"),
+        responsibilities: [
+          "Developed and maintained various company website projects.",
+          "Handled both frontend and backend development tasks.",
+          "Integrate third-party APIs and applications to support marketing initiatives.",
+          "Created and managed robust Content Management Systems (CMS).",
+          "Designed and implemented APIs for seamless integration.",
+        ],
+      },
+      {
+        companyName: "PT Hamid Jaya Abadi",
+        jobTitle: "Fullstack Developer",
+        startDate: new Date("2023-09-01"),
+        endDate: new Date("2023-11-31"),
+        responsibilities: [
+          "Manages team for web development of tour agency by PT. HAMID JAYA ABADI.",
+          "Involvement with code and development activities (debug, deploy, etc).",
+        ],
+      },
+      {
+        companyName: "CV. Nabila Zafira Mahalia",
+        companyAlias: "NaZMa Office",
+        jobTitle: "Fullstack Web Developer",
+        startDate: new Date("2022-07-01"),
+        endDate: new Date("2022-12-31"),
+        responsibilities: [
+          "Built official website for the company from the ground up.",
+          "Complete with frontend, backend, and database utilization.",
+        ],
+      },
+    ],
+  },
+};
