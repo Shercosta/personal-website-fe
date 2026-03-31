b8469ea7c5810dd548ddec97b5fa9f562e08159c
Shercosta <shercostagp@gmail.com>
2026-03-31 15:32:40 +0700

    feat: update projects to include repos

--- src/arrays/project.js
+++ src/arrays/project.js
@@ -10,6 +10,17 @@ let projects = [
     image:
       "https://raw.githubusercontent.com/Shercosta/personal-website-fe/refs/heads/main/public/projects/drxwear.jpg",
     link: "https://drxwear.com",
+    repos: [
+      {
+        title: "DRX Wear CMS & API",
+        items: [
+          "Developed robust APIs to manage CRUD (Create, Read, Update, Delete) operations for products and blogs using Strapi.",
+          "Integrated with Cloud Storage MiniO to store media such as images and videos.",
+          "Designed a scalable database within Strapi's content builder along with the relations.",
+        ],
+        technologiesUsed: ["Strapi", "MiniO", "PostgreSQL", "Postman"],
+      },
+    ],
   },
   {
     _id: {
@@ -22,6 +33,22 @@ let projects = [
     image:
       "https://raw.githubusercontent.com/Shercosta/personal-website-fe/refs/heads/main/public/projects/drxtoken.jpg",
     link: "https://drxtoken.com",
+    repos: [
+      {
+        title: "DRX Token - Landing Page & CMS",
+        items: [
+          "Developed robust APIs to manage CRUD (Create, Read, Update, Delete) operations for blogs, events, news, and the community hub.",
+          "Integrated with Google Cloud Console to fetch and display YouTube video information on the website.",
+        ],
+        technologiesUsed: [
+          "React Js",
+          "Hono",
+          "PostgreSQL",
+          "Postman",
+          "Prisma",
+        ],
+      },
+    ],
   },
   {
     _id: {
@@ -33,6 +60,25 @@ let projects = [
       "Internal System for Bursa Efek Indonesia, built to help maintain and keep an eye on the stock market, emitens, and companies willing to change their market/company status.",
     image:
       "https://raw.githubusercontent.com/Shercosta/personal-website-fe/refs/heads/main/public/idx-bei.jpg",
+    repos: [
+      {
+        title: "PT Bursa Efek Indonesia",
+        items: [
+          "Developed routes and endpoints for pipelines.",
+          "Developed features to import data from excel to information system records.",
+          "Enhanced request and response for better developer experience on the backend side.",
+        ],
+        technologiesUsed: [
+          "Go",
+          "PostgreSQL",
+          "Postman",
+          "Linux",
+          "Docker",
+          "Redis",
+          "Node Js",
+        ],
+      },
+    ],
   },
   {
     _id: {
@@ -44,6 +90,29 @@ let projects = [
       "Information System for Mandiri Utama Finance's employee to keep track of potential frauds, losses, and how to resolve them regarding their credit services.",
     image:
       "https://raw.githubusercontent.com/Shercosta/personal-website-fe/refs/heads/main/public/muf.jpg",
+    repos: [
+      {
+        title: "PT Mandiri Utama Finance - ORM System",
+        items: [
+          "Designed and developed a reusable dynamic form component, enabling standardized form usage across the platform to enhance scalability and maintainability.",
+          "Implemented a robust notification system integrating email and CMS alerts to ensure timely updates for both citizens and administrators.",
+          "Enhanced the audit trail feature to meticulously track and log changes, promoting transparency and accountability within the system.",
+          "Developed base code templates to streamline the development of similar features, establishing best practices for future developers and ensuring code consistency.",
+        ],
+        technologiesUsed: [
+          "PHP",
+          "TypeScript",
+          "JavaScript",
+          "Laravel",
+          "React Js",
+          "PostGreSQL",
+          "Postman",
+          "Linux",
+          "Docker",
+          "Node Js",
+        ],
+      },
+    ],
   },
   {
     _id: {
@@ -56,6 +125,51 @@ let projects = [
     image:
       "https://raw.githubusercontent.com/Shercosta/personal-website-fe/refs/heads/main/public/sportnet.jpg",
     link: "https://drxsportnet.id",
+    repos: [
+      {
+        title: "DRX - Sportsnet",
+        items: [
+          "Created 8 separate algorithms to generate padel matches based on the amount of players or teams, divided by genders and groups.",
+          "Engineered the Padel Hub Tournament Organizer feature for mobile and backend systems, including comprehensive database design.",
+          "Integrated the Pandascore API to display real-time esports match data within the application.",
+          "Integrated with SportsDataIo API to display MMA UFC match data within the application.",
+        ],
+        technologiesUsed: [
+          "React Native",
+          "Hono",
+          "PostgreSQL",
+          "Postman",
+          "Prisma",
+        ],
+      },
+      {
+        title: "DRX - Master Service",
+        items: [
+          "Developed and implemented APIs enabling users to exchange in-app points for crypto tokens, which can then be sent to an external crypto wallet.",
+          "Engineered APIs for a feature that allows users to redeem crypto tokens for real-life rewards (e.g., clothing, game currency).",
+          "Designed and built APIs to allow admins to manage features and rewards within the system.",
+          "Created a Web3 transaction system using ethers.js and RPC providers like Infura and Alchemy to facilitate user redemptions.",
+          "Integrated with the CoinMarketCap API to dynamically adjust the token price for redeemed items based on their conversion rate to Indonesian Rupiah (IDR).",
+          "Integrated with Indodax API to retrieve the latest DRX Token price in IDR currency.",
+          "Integrated Discord OAuth2 to let users connect their Discord account to the application for more rewards.",
+        ],
+        technologiesUsed: [
+          "Typescript",
+          "Web3 Js",
+          "Ethers Js",
+          "Infura",
+          "Alchemy",
+          "CoinMarketCap API",
+          "Hono",
+          "PostgreSQL",
+          "Postman",
+          "Prisma",
+          "Redis",
+          "Discord API",
+          "Indodax API",
+        ],
+      },
+    ],
   },
   {
     _id: {
@@ -67,6 +181,25 @@ let projects = [
       "Public and Internal Information System for the government of Semarang City to resolve and track the progress of the citizens' public complaints.",
     image:
       "https://raw.githubusercontent.com/Shercosta/personal-website-fe/refs/heads/main/public/sapambakita.jpg",
+    repos: [
+      {
+        title:
+          "Sapa Mbak Ita (Governor Public Reports Service for Semarang Region)",
+        items: [
+          "Developed a public reporting platform for citizens to submit concerns and complaints to the local government.",
+          "Worked on both frontend and backend components, including real-time report tracking and admin management features.",
+        ],
+        technologiesUsed: [
+          "Go",
+          "Laravel",
+          "PHP",
+          "JavaScript",
+          "Node Js",
+          "PostgreSQL",
+          "Vue",
+        ],
+      },
+    ],
   },
   {
     _id: {
@@ -78,6 +211,44 @@ let projects = [
       "Information System for Lavees Beauty Clinic, with integration for payment gateway such as BCA, Gojek, etc.",
     image:
       "https://raw.githubusercontent.com/Shercosta/personal-website-fe/refs/heads/main/public/projects/lavees.png",
+    repos: [
+      {
+        title: "Lavees Clinic Beauty & Wellness",
+        items: [
+          "Built an e-commerce platform for booking beauty treatments and purchasing wellness products.",
+          "Integrated third-party payment gateways and designed a user-centric frontend with responsive design.",
+        ],
+        technologiesUsed: [
+          "PHP",
+          "JavaScript",
+          "Laravel",
+          "React Js",
+          "PostgreSQL",
+          "Postman",
+          "Linux",
+          "Docker",
+          "Node Js",
+        ],
+      },
+      {
+        title: "Nuxa Skin Clinic",
+        items: [
+          "Managed the backend for the clinic’s appointment scheduling system and developed user dashboards for clients to track their treatment history.",
+          "Ensured scalability and data security compliance for patient information.",
+        ],
+        technologiesUsed: [
+          "PHP",
+          "JavaScript",
+          "Laravel",
+          "React Js",
+          "PostgreSQL",
+          "Postman",
+          "Linux",
+          "Docker",
+          "Node Js",
+        ],
+      },
+    ],
   },
   {
     _id: {
@@ -89,6 +260,46 @@ let projects = [
       "Internal Information System to manage, track, and resolve their loans for the partnering companies.",
     image:
       "https://raw.githubusercontent.com/Shercosta/personal-website-fe/refs/heads/main/public/projects/site-visit-smi.png",
+    repos: [
+      {
+        title: "PT Sarana Multi Infrastruktur (Persero) - 'Site Visit'",
+        items: [
+          "Enhanced the CMS and API to handle detailed facility or collateral visit information.",
+          "Developed a map function for users to add precise pin-point locations of visits.",
+          "Integrated user import and sync functionality from SMI Maxflow.",
+        ],
+        technologiesUsed: [
+          "PHP",
+          "JavaScript",
+          "Laravel",
+          "Vue",
+          "Microsoft SQL Server",
+          "Postman",
+          "Linux",
+          "Docker",
+          "Node Js",
+        ],
+      },
+      {
+        title: "PT Sarana Multi Infrastruktur (Persero) - 'Refina'",
+        items: [
+          "Created a view-only section in Refina's CMS to display data generated in the 'Site Visit' project.",
+          "Ensured seamless integration between the two projects for improved data visibility.",
+          "Integrated user import and sync functionality from SMI Maxflow.",
+        ],
+        technologiesUsed: [
+          "PHP",
+          "JavaScript",
+          "Laravel",
+          "Vue",
+          "Microsoft SQL Server",
+          "Postman",
+          "Linux",
+          "Docker",
+          "Node Js",
+        ],
+      },
+    ],
   },
   {
     _id: {
